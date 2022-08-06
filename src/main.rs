265d3bbdcab8e308032456524a9d29ac8903642d
Alex <alex@alexaa.dev>
2022-08-06 16:48:30 -0300

    feat: accepts connections
    sends back hello world to new connections

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::io;
+use tokio::io::AsyncWriteExt;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    // For some reason, 'localhost' is not the same as '127.0.0.1'.
+    // Using 'localhost' gives UB (kinda?).
+    let server_addr = "127.0.0.1:5445";
+    let listener = TcpListener::bind(server_addr).await?;
+    println!("Listening on '{}'...", server_addr);
+
+    loop {
+        let (mut stream, socket_addr) = listener.accept().await?;
+        println!("User connected: '{:?}'", socket_addr);
+
+        stream.write_all(b"hello world").await?;
+    }
 }
